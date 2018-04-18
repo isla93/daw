2a75c67260ae39460e782f1af1a46c2cbd5c2b2b
Vespertino <Vespertino@C8PC10>
2018-04-18 17:19:41 +0200

    kk

--- src/daw/hola.java
+++ src/daw/hola.java
@@ -3,7 +3,7 @@ package daw;
 public class hola {
 
 	public static void main(String[] args) {
-		System.out.println("hdfgdfgfgvjhkjhhdsola");
+		System.out.println("hdfgdfgfgvjhkkkjhhdsola");
 
 	}
 
